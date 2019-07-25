5e2416bbb9e708626a157b8b574f7cba8b947c84
Roman Sologor <solohor.roman@gmail.com>
2019-07-25 10:54:33 +0300

    Add second card to the screen with additional getstures

--- designCode/design_code/screens/ProjectsScreen.js
+++ designCode/design_code/screens/ProjectsScreen.js
@@ -3,36 +3,68 @@ import { Animated, PanResponder } from 'react-native';
 import styled from 'styled-components';
 
 import Project from '../components/Project';
+import { projects } from '../components/data';
 
 export default class ProjectsScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       pan: new Animated.ValueXY(),
+      scale: new Animated.Value(0.9),
+      translateY: new Animated.Value(44),
     };
 
     const {
       pan,
       pan: { x, y },
+      scale,
+      translateY,
     } = this.state;
 
     this._panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: () => true,
-      onPanResponderRelease: () => {
-        Animated.spring(pan, {
-          toValue: { x: 0, y: 0 },
-        }).start();
-      },
       onPanResponderMove: Animated.event([
         null,
         { dx: x, dy: y },
       ]),
+      onPanResponderGrant: () => {
+        Animated.spring(scale, {
+          toValue: 1,
+        }).start();
+        Animated.spring(translateY, {
+          toValue: 0,
+        }).start();
+      },
+      onPanResponderRelease: () => {
+        const yPosition = y.__getValue();
+
+        if (yPosition > 200) {
+          Animated.timing(pan, {
+            toValue: {
+              x,
+              y: 1000,
+            },
+          }).start();
+        } else {
+          Animated.spring(pan, {
+            toValue: { x: 0, y: 0 },
+          }).start();
+          Animated.spring(scale, {
+            toValue: 0.9,
+          }).start();
+          Animated.spring(translateY, {
+            toValue: 44,
+          }).start();
+        }
+      },
     });
   }
 
   render() {
     const {
       pan: { x, y },
+      scale,
+      translateY,
     } = this.state;
 
     return (
@@ -47,10 +79,30 @@ export default class ProjectsScreen extends Component {
           {...this._panResponder.panHandlers}
         >
           <Project
-            title="Price Tag"
-            image={require('../assets/background5.jpg')}
-            author="Liu Yi"
-            text="Thanks to Design+Code, I improved my design skill and learned to do animations for my app Price Tag, a top news app in China."
+            title={projects[0].title}
+            image={projects[0].image}
+            author={projects[0].author}
+            text={projects[0].text}
+          />
+        </Animated.View>
+        <Animated.View style={{
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          justifyContent: 'center',
+          alignItems: 'center',
+          zIndex: -1,
+          transform: [
+            { scale },
+            { translateY },
+          ],
+        }}
+        >
+          <Project
+            title={projects[1].title}
+            image={projects[1].image}
+            author={projects[1].author}
+            text={projects[1].text}
           />
         </Animated.View>
       </Container>
